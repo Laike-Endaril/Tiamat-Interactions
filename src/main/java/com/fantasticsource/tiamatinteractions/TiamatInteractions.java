79afd69d8c2db51173af8879ea245ccff13a91f0
Kyle Koder <silvertallon@gmail.com>
2021-02-27 17:58:45 -0800

    1.12.2.000d

--- src/main/java/com/fantasticsource/tiamatinteractions/TiamatInteractions.java
+++ src/main/java/com/fantasticsource/tiamatinteractions/TiamatInteractions.java
@@ -26,7 +26,7 @@ public class TiamatInteractions
 {
     public static final String MODID = "tiamatinteractions";
     public static final String NAME = "Tiamat Interactions";
-    public static final String VERSION = "1.12.2.000c";
+    public static final String VERSION = "1.12.2.000d";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
